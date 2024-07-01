112a5c4a556cd58dac1b3efe95917aa1189692d4
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-07-01 23:58:14 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -13,13 +13,9 @@
         <title>Galle TN Store</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- latest compiled and minified CSS -->
         <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
-        <!-- jquery library -->
         <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
-        <!-- Latest compiled and minified javascript -->
         <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-        <!-- External CSS -->
         <link rel="stylesheet" href="css/style.css" type="text/css">
     </head>
     <body href="img/1.jpg">
