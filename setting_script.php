90642398e6fe29be10b57a742f9df7e0ba952485
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-07-01 23:57:34 +0530

    Update setting_script.php

--- setting_script.php
+++ setting_script.php
@@ -7,11 +7,11 @@
     $old_password= md5(md5(mysqli_real_escape_string($con,$_POST['oldPassword'])));
     $new_password= md5(md5(mysqli_real_escape_string($con,$_POST['newPassword'])));
     $email=$_SESSION['email'];
-    //echo $email;
+
     $password_from_database_query="select password from users where email='$email'";
     $password_from_database_result=mysqli_query($con,$password_from_database_query) or die(mysqli_error($con));
     $row=mysqli_fetch_array($password_from_database_result);
-    //echo $row['password'];
+
     if($row['password']==$old_password){
         $update_password_query="update users set password='$new_password' where email='$email'";
         $update_password_result=mysqli_query($con,$update_password_query) or die(mysqli_error($con));
@@ -26,6 +26,5 @@
         </script>
         <meta http-equiv="refresh" content="1;url=settings.php" />
         <?php
-        //header('location:settings.php');
     }
 ?>
