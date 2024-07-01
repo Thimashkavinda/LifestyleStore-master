cf5938e72910f0333353c44a158c66dcf8aa0da5
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-07-01 23:56:12 +0530

    Update login_submit.php

--- login_submit.php
+++ login_submit.php
@@ -28,12 +28,10 @@
         </script>
         <meta http-equiv="refresh" content="1;url=login.php" />
         <?php
-        //header('location: login');
-        //echo "Wrong email or password.";
     }else{
         $row=mysqli_fetch_array($user_authentication_result);
         $_SESSION['email']=$email;
-        $_SESSION['id']=$row['id'];  //user id
+        $_SESSION['id']=$row['id']; 
         header('location: products.php');
     }
     
