ab9b8eac9f07cb5585d591a981d567a8871a2802
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-07-01 23:59:11 +0530

    Update store.sql

--- store.sql
+++ store.sql
@@ -1,30 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 11, 2017 at 04:06 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `store`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `items`
---
 
 CREATE TABLE `items` (
   `id` int(11) NOT NULL,
@@ -32,9 +11,6 @@ CREATE TABLE `items` (
   `price` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `items`
---
 
 INSERT INTO `items` (`id`, `name`, `price`) VALUES
 (1, 'Cannon EOS', 36000),
@@ -50,11 +26,7 @@ INSERT INTO `items` (`id`, `name`, `price`) VALUES
 (11, 'HXR', 900),
 (12, 'PINK', 1200);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -66,20 +38,13 @@ CREATE TABLE `users` (
   `address` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `city`, `address`) VALUES
 (1, 'Sajal', 'sajal.agrawal1997@gmail.com', '57f231b1ec41dc6641270cb09a56f897', '8899889988', 'Indore', '100 palace colony, Indore'),
 (2, 'Ram', 'ram1234@xyz.com', '57f231b1ec41dc6641270cb09a56f897', '8899889989', 'Pune', '100 palace colony, Pune'),
 (3, 'Shyam', 'shyam@xyz.com', '57f231b1ec41dc6641270cb09a56f897', '8899889990', 'Bangalore', '100 palace colony, Bangalore');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users_items`
---
 
 CREATE TABLE `users_items` (
   `id` int(11) NOT NULL,
@@ -88,9 +53,7 @@ CREATE TABLE `users_items` (
   `status` enum('Added to cart','Confirmed') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users_items`
---
+
 
 INSERT INTO `users_items` (`id`, `user_id`, `item_id`, `status`) VALUES
 (7, 3, 3, 'Added to cart'),
@@ -101,60 +64,34 @@ INSERT INTO `users_items` (`id`, `user_id`, `item_id`, `status`) VALUES
 (12, 1, 2, 'Added to cart'),
 (13, 1, 8, 'Added to cart');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `items`
---
 ALTER TABLE `items`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `users`
---
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `users_items`
---
+
 ALTER TABLE `users_items`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`,`item_id`),
   ADD KEY `item_id` (`item_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `items`
---
+
 ALTER TABLE `items`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT for table `users`
---
+
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `users_items`
---
+
 ALTER TABLE `users_items`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `users_items`
---
+
 ALTER TABLE `users_items`
   ADD CONSTRAINT `users_items_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `users_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
