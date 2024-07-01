137bbf1c441539dededfd2b190492608047de4d4
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-07-01 23:53:18 +0530

    Update check_if_added.php

--- check_if_added.php
+++ check_if_added.php
@@ -1,7 +1,6 @@
 <?php
     
     function check_if_added_to_cart($item_id){
-        //session_start();    
         require 'connection.php';
         $user_id=$_SESSION['id'];
         $product_check_query="select * from users_items where item_id='$item_id' and user_id='$user_id' and status='Added to cart'";
