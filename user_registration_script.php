9567a19e5735089c2c839f238fbbe728c906d4b6
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-07-01 23:59:57 +0530

    Update user_registration_script.php

--- user_registration_script.php
+++ user_registration_script.php
@@ -24,8 +24,7 @@
     $duplicate_user_result=mysqli_query($con,$duplicate_user_query) or die(mysqli_error($con));
     $rows_fetched=mysqli_num_rows($duplicate_user_result);
     if($rows_fetched>0){
-        //duplicate registration
-        //header('location: signup.php');
+       
         ?>
         <script>
             window.alert("Email already exists in our database!");
@@ -34,13 +33,10 @@
         <?php
     }else{
         $user_registration_query="insert into users(name,email,password,contact,city,address) values ('$name','$email','$password','$contact','$city','$address')";
-        //die($user_registration_query);
         $user_registration_result=mysqli_query($con,$user_registration_query) or die(mysqli_error($con));
         echo "User successfully registered";
         $_SESSION['email']=$email;
-        //The mysqli_insert_id() function returns the id (generated with AUTO_INCREMENT) used in the last query.
         $_SESSION['id']=mysqli_insert_id($con); 
-        //header('location: products.php');  //for redirecting
         ?>
         <meta http-equiv="refresh" content="3;url=products.php" />
         <?php
